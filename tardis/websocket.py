7bef70fd8624cd3280160f7941e0eb5674fcda0b
aamingaa <774066625@qq.com>
2025-09-02 22:28:02 +0800

    hello

--- tardis/websocket.py
+++ tardis/websocket.py
@@ -4,60 +4,54 @@ Copyright (C) 2017-2025 Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
-from copy import deepcopy
+import time
 
 from cryptofeed import FeedHandler
 from cryptofeed.callback import BookCallback
-from cryptofeed.defines import L2_BOOK
-from cryptofeed.exchanges import Coinbase,BinanceFutures, Binance
+from cryptofeed.defines import BID, ASK, L2_BOOK
+from cryptofeed.exchanges import Bitmex, BinanceFutures
 
 
-PREV = {}
 counter = 0
+avg = 0
+START = time.time()
+STATS = 1000
 
 
 async def book(feed, symbol, book, timestamp):
-    global PREV
     global counter
-    if book == PREV:
-        print("Current")
-        print(book)
-        print("\n\n")
-        print("Previous")
-        print(PREV)
-    assert book != PREV
-    PREV = deepcopy(book)
+    global avg
+
+    t = time.time()
     counter += 1
-    if counter % 10 == 0:
-        print(".", end='', flush=True)
+    bids = list(book[BID].keys())
+    asks = list(book[ASK].keys())
+    avg += (t - timestamp)
 
-async def handle_orderbook(client, timestamp, symbol, book, checksum,** kwargs):
-    """处理订单簿数据的回调函数"""
-    print(f"Timestamp: {timestamp} Symbol: {symbol} Book: {book} Checksum: {checksum}")
+    try:
+        assert (t - timestamp) < 2
+        assert bids[-1] < asks[0]
+    except Exception:
+        print("FAILED")
+        print("BID", bids[-1])
+        print("ASKS", asks[0])
+        print("DELTA", t - timestamp)
+        print("COUNTER", counter)
 
-    
+    if counter % STATS == 0:
+        print("Checked", counter, "updates")
+        print("Runtime", t - START)
+        print("Current spread", asks[0] - bids[-1])
+        print("Average book update handle time", avg / counter)
+        print("\n")
+
 
 def main():
     f = FeedHandler()
-    try:
-        f.add_feed(BinanceFutures(max_depth=5, symbols=['ETH-USDT-PERP'], channels=[L2_BOOK], callbacks={L2_BOOK: BookCallback(book)}))
-        f.run()
-    except Exception as e:
-        print("Connection reset by peer, retrying...")
 
-def check_symbol(symbol, exchange):
-    """检查交易对是否被交易所支持"""
-    try:
-        exchange_info = exchange.info()
-        symbols = exchange_info['symbols']
-        print(symbols)
-        # supported = any(s['symbol'] == symbol for s in symbols)
-        # return supported
-    except Exception as e:
-        print(f"获取信息出错: {e}")
-        # return False
-    
+    f.add_feed(BinanceFutures(symbols=['ETH-USDT-PERP'], channels=[L2_BOOK], callbacks={L2_BOOK: BookCallback(book)}))
+    f.run()
+
+
 if __name__ == '__main__':
-    # binance_futures = BinanceFutures()
-    # check_symbol('ETHUSDT', binance_futures)
     main()
